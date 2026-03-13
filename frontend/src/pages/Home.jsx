a9456f1b7ba35f96506214464aa6d30d1a52a220
Tanishk Panchal <panchaltanishk03@gmail.com>
2026-03-13 17:36:18 +0530

    backend almost done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,37 @@
-import { useState } from "react"
-
+import {  useEffect, useState } from "react"
+import axios from 'axios'
 function Home(){
 
     const[todos , setTodos] = useState([])
     const[error , setError]=useState(null)
     const[loading , setLoading]=useState(false)
+
+    useEffect(()=>{
+        const fetchTodo = async ()=>{
+            try {
+                setLoading(true)
+                const response = await axios.get("http://localhost:3000/todo/home",{
+                    withCredentials:true,
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+                    
+                })
+                setTodos(response.data.todos)
+                setError(null)
+            } catch (error) {
+                setError("Failed to fetch todos")
+            }finally{
+                setLoading(false)
+            }
+        }
+        fetchTodo()
+    },[])
+
     return(
         <h1>Home page</h1>
+
+       
     )
 }
 
