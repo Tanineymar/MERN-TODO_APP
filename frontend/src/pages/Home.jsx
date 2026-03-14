5426e2b27cf574ff366f8b716828429c55cb684e
Tanishk Panchal <panchaltanishk03@gmail.com>
2026-03-14 23:50:44 +0530

    Register page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
 function Home(){
+
+    const [loading , setLoading]=useState(false)
+    const [todos , setTodos]=useState([ ])
+    const [error , setError]=useState(null)
+
+    useEffect(()=>{
+        const fetchtodos = async () => {
+           try {
+            setLoading(true)
+             const response = await axios.get("http://localhost:3000/todo/home",{
+                withCredentials :true,
+                headers:{
+                    "Content-Type":"application/json"
+                }
+             })
+             setTodos(response.data.todos)
+             setError(null)
+           } catch (error) {
+             setError("Failed to fetch todos")
+           }finally{
+            setLoading(false)
+           }
+        }
+        fetchtodos();
+    },[])
     return(
         <div>
             <h1>HOME PAGE</h1>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
 function Register(){
+    const[username , setUsername]=useState("")
+    const[email , setEmail]=useState("")
+    const[password ,setPassword]=useState("")
+
+    const handleRegister =(event)=>{
+        event.preventDefault();
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return(
         <div>
-            Register Page
+            <div>
+                <h2>Register</h2>
+            </div>
+            <div>
+                <form onSubmit={handleRegister} method="post">
+                    <div>
+                        <label htmlFor="Username">Username</label>
+                        <input onChange={(event)=>setUsername(event.target.value)} type="text" placeholder="Enter name" />
+                    </div>
+                    <div>
+                        <label htmlFor="Email">Email</label>
+                        <input type="email" placeholder="Enter email" />
+                    </div>
+                    <div>
+                        <label htmlFor="Password">Password</label>
+                        <input type="password" placeholder="Min. 6 Characters" />
+                    </div>
+                    <button>Create account</button>
+                    <p>Already have an account? <Link to="/login">Login</Link></p>
+                </form>
+            </div>
         </div>
     )
 }
