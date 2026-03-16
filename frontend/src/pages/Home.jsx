e25e76c8debcf41a41e5c5d0f7ea6bbad3520814
Tanishk Panchal <panchaltanishk03@gmail.com>
2026-03-16 15:44:08 +0530

    Register CSS done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
-import { data } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
 function Home(){
+    const navigate = useNavigate()
 
     const [loading , setLoading]=useState(false)
     const [todos , setTodos]=useState([ ])
     const [error , setError]=useState(null)
 
     useEffect(()=>{
+
         const fetchtodos = async () => {
            try {
             setLoading(true)
+            const token =  localStorage.getItem("token")
+            if(!token){
+                navigate("/login")
+            }
              const response = await axios.get("http://localhost:3000/todo/home",{
                 withCredentials :true,
                 headers:{
+                    Authorization:`Bearer ${token}`,
                     "Content-Type":"application/json"
                 }
              })
              setTodos(response.data.todos)
+            //  alert(response.data.message)
              setError(null)
            } catch (error) {
              setError("Failed to fetch todos")
@@ -31,6 +40,13 @@ function Home(){
     return(
         <div>
             <h1>HOME PAGE</h1>
+            {loading && <p>Loading...</p>}
+            {
+                todos.map((todo, id)=>(
+                    <p key={id}>{todo.title}
+                    {todo.description}</p>
+                ))
+            }
         </div>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,57 +2,96 @@ import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import axios from 'axios'
 
-function Register(){
+function Register() {
     const navigate = useNavigate()
 
-    const[name , setName]=useState("")
-    const[email , setEmail]=useState("")
-    const[password ,setPassword]=useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    const handleRegister = async(event)=>{
+    const handleRegister = async (event) => {
         event.preventDefault();
         try {
-            const {data} = await axios.post("http://localhost:3000/api/auth/register" , {
+            const response = await axios.post("http://localhost:3000/api/auth/register", {
                 name,
                 email,
                 password,
-            },{
-                withCredentials : true,
-                headers:{
-                    "Content-type":"application/json"
+            }, {
+                withCredentials: true,
+                headers: {
+                    "Content-type": "application/json"
                 }
             })
             console.log(data)
             alert("User registered successfully")
             localStorage.setItem("token", data.token)
-            navigate("/login")
+            setName("")
+            setEmail("")
+            setPassword("")
+            navigate("/home")
         } catch (error) {
             console.log(error)
+            alert("Registration failed")
+             setName("")
+            setEmail("")
+            setPassword("")
         }
     }
 
-    return(
-        <div>
-            <div>
-                <h2>Register</h2>
+    return (
+        <div className="flex min-h-screen">
+            {/* Left side div*/}
+            <div className="hidden lg:flex w-1/2 bg-gradient-to-br from-violet-900 to-indigo-600 text-white p-16 flex-col justify-center">
+                <h1 className="text-3xl mb-10 font-semibold ">Taskly</h1>
+                <h1 className="text-5xl font-serif leading-relaxed">
+                    Oraganize your tasks,<br />
+                    <span className="italic text-fuchsia-300">effortlessly.</span>
+                </h1>
+                <p className="mt-6 text-gray-300 max-w-md">Join thousands of people who use TodoApp to stay productive and never miss what matters.</p>
+                <ul className="mt-30 space-y-4 text-gray-300">
+                    <li>• Create and manage todos instantly</li>
+                    <li>• Secure authentication & data storage</li>
+                    <li>• Access from any device, anytime</li>
+                </ul>
             </div>
-            <div>
-                <form onSubmit={handleRegister} method="post">
-                    <div>
-                        <label htmlFor="Username" >Username</label>
-                        <input onChange={(event)=>setName(event.target.value)} value={name} id="Username" type="text" placeholder="Enter name" />
-                    </div>
-                    <div>
-                        <label htmlFor="Email" >Email</label>
-                        <input onChange={(event)=>setEmail(event.target.value)} value={email} id="Email" type="email" placeholder="Enter email" />
-                    </div>
-                    <div>
-                        <label htmlFor="Password" >Password</label>
-                        <input onChange={(event)=>setPassword(event.target.value)} value={password} id="Password" type="password" placeholder="Min. 6 Characters" />
-                    </div>
-                    <button type="submit">Create account</button>
-                    <p>Already have an account? <Link to="/login">Login</Link></p>
-                </form>
+
+            {/* Right side div */}
+            <div className="flex w-full lg:w-1/2 items-center justify-center bg-black text-white p-8" >
+                < div className="w-full max-w-md bg-gray-900 px-8 py-10 rounded-2xl">
+
+                    <h2 className="text-4xl font-serif leading-relaxed mb-2">Create account</h2>
+
+                    <p className="text-gray-400 mb-8 ">
+                        Already have one?
+                        <Link to="/login" className="text-indigo-400 ml-2">
+                            Login
+                        </Link>
+                    </p>
+
+                    <form onSubmit={handleRegister} method="post" className="space-y-5">
+                        <div className="flex-gap-4">
+                            <label htmlFor="Username">Username</label>
+                            <input onChange={(event) => setName(event.target.value)} value={name} id="Username" type="text" placeholder="Enter name" 
+                            className="w-full p-3 mt-1.5 rounded-md bg-gray-800 border border-gray-700 focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Email" >Email</label>
+                            <input onChange={(event) => setEmail(event.target.value)} value={email} id="Email" type="email" placeholder="Enter email"
+                            className="w-full p-3 mt-1.5 rounded-md bg-gray-800 border border-gray-700 focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="Password" >Password</label>
+                            <input onChange={(event) => setPassword(event.target.value)} value={password} id="Password" type="password" placeholder="Min. 6 Characters"
+                            className="w-full p-3 mt-1.5 rounded-md bg-gray-800 border border-gray-700 focus:outline-none focus:border-indigo-500"
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-indigo-600 hover:bg-indigo-800 transition p-3 rounded-md font-semibold">Create account</button>
+
+                    </form>
+
+                </div>
             </div>
         </div>
     )
