ad222fdb32a06a30069b109c1a31a46a4700d178
Tanishk Panchal <panchaltanishk03@gmail.com>
2026-03-25 17:24:34 +0530

    Edit json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 
 
 import express from "express"
-import cors from 'cors'
+
 import cookieParser from "cookie-parser"
 import authRouter from './routes/auth.routes.js'
 import todoRouter from './routes/todo.routes.js'
